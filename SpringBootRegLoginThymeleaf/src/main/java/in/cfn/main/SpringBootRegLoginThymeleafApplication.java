e9d0070331b22a859b583cd2d558ad5472cb6327
CodeForgeNet <codeforgenet@icloud.com>
2025-08-03 15:29:39 +0530

    create loginReg thymeleaf Project

---
+++ SpringBootRegLoginThymeleaf/src/main/java/in/cfn/main/SpringBootRegLoginThymeleafApplication.java
@@ -0,0 +1,13 @@
+package in.cfn.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegLoginThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRegLoginThymeleafApplication.class, args);
+	}
+
+}
